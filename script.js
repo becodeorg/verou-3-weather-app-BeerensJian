4e81f925392a5618e4c4a3881ca7b053ee7c07c5
JianB <Beerensjian@gmail.com>
2022-01-25 16:08:52 +0100

    generate card header element for each day

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ button.addEventListener("click", (e) => {
                 For each day generate a card with elements gethered from the OpenWeather API.
                 daydate.getDate() -> get the day of the month
                 daydate.getDay() -> returns a value 0-6 corresponding to day in the week
-                TODO: Append name of the weekday, number of day in the month, and month in dateinfo
+                TODO: Grab the description, icon, temp and feels like temp and append them to NewCard
             */
             for (let i = 0; i < weekdata.length ; i++) {
                 const newCard = document.createElement("div");         
@@ -41,12 +41,25 @@ button.addEventListener("click", (e) => {
                 dayHeader.className = "dateinfo"
                 const cardTitle = document.createElement("p")
                 cardTitle.className = "day"
+                const dayInMonth = document.createElement("p")
+                dayInMonth.className = "dayinmonth"
+                const month = document.createElement("p")
+                month.className = "month"
                 
                 const unixtime = weekdata[i].dt;
                 const daydate = new Date(unixtime * 1000); // converts the unix time to miliseconds unix time so it can work with Date()
                 
+                cardTitle.innerText = getNameDay(daydate);
+                dayInMonth.innerText = daydate.getDate();
+                month.innerText = getNameMonth(daydate);
+                dayHeader.append(cardTitle, dayInMonth, month);
+                newCard.appendChild(dayHeader);
 
 
+
+
+
+                cardcontainer.appendChild(newCard);
 
 
 
@@ -58,7 +71,12 @@ button.addEventListener("click", (e) => {
 
 })
 
-function getNameDay(dayNumber) {
-    names = ["Sunday", "Monday", "Thuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    return names[dayNumber.getDay()]
+function getNameDay(datee) {
+    const names = ["Sunday", "Monday", "Thuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    return names[datee.getDay()]
+}
+
+function getNameMonth(datee) {
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    return months[datee.getMonth()]
 }
